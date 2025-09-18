bb9ec36e7078cb2eb97d7ab95d9412f942c332f8
blackbydefault <119285013+blackbydefault@users.noreply.github.com>
2025-09-17 21:08:57 -0400

    ASCII Art for Step 1 Complete
    Added print statement of first/last name initials in ASCII art.

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,5 +1,13 @@
 public class Assignment1Program1 {
   public static void main(String[] args) {
-
-  }
+System.out.println(
+      "LLL                AAA      \n" +
+      "LLL               AAAAA     \n" +
+      "LLL              AAA AAA    \n" +
+      "LLL             AAA   AAA   \n" +
+      "LLL            AAAAAAAAAAA  \n" +
+      "LLLLLLLLLLL   AAA       AAA \n" +
+      "LLLLLLLLLLL  AAA         AAA"
+    );
+}
 }
