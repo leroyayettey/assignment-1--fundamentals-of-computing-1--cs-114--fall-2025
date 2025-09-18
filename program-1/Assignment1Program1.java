44dfbfaa4f21484686f941b20e939481751991f0
blackbydefault <119285013+blackbydefault@users.noreply.github.com>
2025-09-18 13:34:11 -0400

    Added random number generation

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Assignment1Program1 {
@@ -15,6 +16,7 @@ public class Assignment1Program1 {
   }
   public static void main(String[] args){
     Assignment1Program1 apObjectOne = new Assignment1Program1();
+    Random rand = new Random();
     System.out.println(
       "LLL                AAA      \n" +
       "LLL               AAAAA     \n" +
@@ -29,8 +31,11 @@ public class Assignment1Program1 {
     String userString = scannerObject.nextLine();
     apObjectOne.reverseText(userString);
     System.out.println("[Please enter a number in Fahrenheit ]: ");
-    Double userFahren = scannerObject.nextDouble();
+    double userFahren = scannerObject.nextDouble();
     apObjectOne.fahrenToCelcius(userFahren);
+    int randomNum = rand.nextInt(32,16385);
+
+
 
 
 
