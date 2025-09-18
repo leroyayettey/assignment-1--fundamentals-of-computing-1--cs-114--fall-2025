992aaafaf946d3261f7b2d3c80d64fe3d830d14a
blackbydefault <119285013+blackbydefault@users.noreply.github.com>
2025-09-17 22:08:08 -0400

    Half-Portion of Step 2 Complete
    Started 5 character input and reverse character method.

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,13 +1,25 @@
+import java.util.Scanner;
+
 public class Assignment1Program1 {
+  String userString;
+  public String reverseText(String inputText){
+    Scanner scannerObject = new Scanner(System.in);
+    System.out.println("[Please enter a 5-character number]: ");
+    userString = scannerObject.nextLine();
+
+
+    return userString;
+  }
+
   public static void main(String[] args) {
-System.out.println(
+    System.out.println(
       "LLL                AAA      \n" +
       "LLL               AAAAA     \n" +
       "LLL              AAA AAA    \n" +
       "LLL             AAA   AAA   \n" +
       "LLL            AAAAAAAAAAA  \n" +
       "LLLLLLLLLLL   AAA       AAA \n" +
-      "LLLLLLLLLLL  AAA         AAA"
+      "LLLLLLLLLLL  AAA         AAA\n"
     );
 }
 }
